05517d5947a8bdb7802d235fbb1719dce732c4fb
simonacossai <cossaisimona@gmail.com>
2020-11-10 17:59:46 +0100

    DONE- fixing the sort function

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-async function loadUsers(){
-    let response= await fetch("https://jsonplaceholder.typicode.com/users",{
+async function loadUsers() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users", {
         "method": "GET",
     })
-    
-    let users= await response.json();
+
+    let users = await response.json();
     return users;
-    }
-    
-    async function createUserCards(){
+}
+
+async function createUserCards() {
     let userObjs = await loadUsers();
     console.log(userObjs);
     let wrapper = document.querySelector(".row");
-    userObjs.forEach(userObj=>
-        {
-            let userCard= document.createElement("div");
-            userCard.className="col-lg-3 col-md-6 col-sm-12 mt-4 user";
-            userCard.innerHTML= `<div class="card" style="width: 16rem;">
+    userObjs.forEach(userObj => {
+        let userCard = document.createElement("div");
+        userCard.className = "col-lg-3 col-md-6 col-sm-12 mt-4 user";
+        userCard.innerHTML = `<div class="card" style="width: 16rem;">
             <div class="card-body">
             <a href="detail.html?id=${userObj.name}|${userObj.username}|${userObj.email}" style="color: black; text-decoration: none">
             <h5 class="card-title">${userObj.name}</h5></a>
@@ -25,50 +24,49 @@ async function loadUsers(){
               <a href="#" class="card-link">Another link</a>
             </div>
           </div>`
-          wrapper.appendChild(userCard);
-        })
-    }
-    createUserCards();
-    
-    
-    async function getName() {
-        let userObjects = await loadUsers();
-        userObjects.forEach(userObject => {
-            console.log(userObject.name)
-        })
-    }
-    
-    // EX-4
-    
-    async function getAddress() {
-        let userObjects = await loadUsers();
-        userObjects.forEach(userObject => {
-            let address = userObject.address;
-            console.log(address.street + ', ' + address.suite + ', ' + address.city + " '" + address.zipcode + "'")
-        })
-    }
-    
-    
+        wrapper.appendChild(userCard);
+    })
+}
+createUserCards();
 
-    async function sortCards() {
-        let cards = document.querySelectorAll(".user");
-    
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].remove();
-        }
-    
-        let userObjs = await loadUsers();
-        console.log(userObjs.sort(function(a,b){
-            let textA = a.username;
-            let textB = b.username;
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        }))
-        let wrapper = document.querySelector(".row");
-        userObjs.forEach(userObj=>
-        {
-            let userCard= document.createElement("div");
-            userCard.className="col-lg-3 col-md-6 col-sm-12 mt-4";
-            userCard.innerHTML=  `<div class="card" style="width: 16rem;">
+
+async function getName() {
+    let userObjects = await loadUsers();
+    userObjects.forEach(userObject => {
+        console.log(userObject.name)
+    })
+}
+
+// EX-4
+
+async function getAddress() {
+    let userObjects = await loadUsers();
+    userObjects.forEach(userObject => {
+        let address = userObject.address;
+        console.log(address.street + ', ' + address.suite + ', ' + address.city + " '" + address.zipcode + "'")
+    })
+}
+
+
+
+async function sortCards() {
+    let cards = document.querySelectorAll(".user");
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].remove();
+    }
+
+    let userObjs = await loadUsers();
+    console.log(userObjs.sort(function (a, b) {
+        let textA = a.username;
+        let textB = b.username;
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    }))
+    let wrapper = document.querySelector(".row");
+    userObjs.forEach(userObj => {
+        let userCard = document.createElement("div");
+        userCard.className = "col-lg-3 col-md-6 col-sm-12 mt-4 user";
+        userCard.innerHTML = `<div class="card" style="width: 16rem;">
             <div class="card-body">
             <h5 class="card-title">${userObj.name}</h5>
               <h6 class="card-subtitle mb-2 text-muted">${userObj.username}</h6>
@@ -77,29 +75,30 @@ async function loadUsers(){
               <a href="#" class="card-link">Another link</a>
             </div>
           </div> `
-          wrapper.appendChild(userCard);
-        })
-    }
-    
-    
-    document.getElementById("search").addEventListener("change", function(event){
-        event.preventDefault()
-        let inputSearch= event.target.value;
-      });
-      
-     document.getElementById("buttonSearch").addEventListener("click", async function getFilteredUser(event){
-        event.preventDefault()
-        let obj = await loadUsers();
-          let input = document.getElementById("search").value;
-          //const result = search(input);
-          //console.log(result);
-          let newArray = [];
+        wrapper.appendChild(userCard);
+    })
+}
+
+
+document.getElementById("search").addEventListener("change", function (event) {
+    event.preventDefault()
+    let inputSearch = event.target.value;
+});
+
+document.getElementById("buttonSearch").addEventListener("click", async function getFilteredUser(event) {
+    event.preventDefault()
+    let obj = await loadUsers();
+    let input = document.getElementById("search").value;
+    let wrapper = document.querySelector(".row");
+
+    //const result = search(input);
+    //console.log(result);
+    let newArray = [];
     for (let i = 0; i < obj.length; i++) {
-        if (obj[i].name.startsWith(input)) {
+        if ((obj[i].name.toLowerCase()).startsWith(input)) {
             newArray.push(obj[i]);
         }
     }
     console.log(newArray);
-    }
-    );
     
+});
---
+++ index2.js
@@ -0,0 +1,147 @@
+async function loadUsers(){
+let response= await fetch("https://jsonplaceholder.typicode.com/users",{
+    "method": "GET",
+})
+
+let users= await response.json();
+return users;
+}
+
+async function createUserCards(){
+let userObjs = await loadUsers();
+// console.log(userObjs);
+let wrapper = document.querySelector(".row");
+userObjs.forEach(userObj=>
+    {
+        let userCard= document.createElement("div");
+        userCard.className="col-lg-3 col-md-6 col-sm-12 mt-4 user";
+        userCard.innerHTML= `<div class="card" style="width: 16rem;">
+        <div class="card-body">
+        <a href="detail.html?id=${userObj.name}|${userObj.username}|${userObj.email}" style="color: black; text-decoration: none">
+        <h5 class="card-title">${userObj.name}</h5></a>
+          <h6 class="card-subtitle mb-2 text-muted">${userObj.username}</h6>
+          <p class="card-text">${userObj.email}</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>`
+      wrapper.appendChild(userCard);
+    })
+}
+createUserCards();
+
+
+async function getName() {
+    let userObjects = await loadUsers();
+    userObjects.forEach(userObject => {
+        console.log(userObject.name)
+    })
+}
+
+// EX-4
+
+async function getAddress() {
+    let userObjects = await loadUsers();
+    userObjects.forEach(userObject => {
+        let address = userObject.address;
+        console.log(address.street + ', ' + address.suite + ', ' + address.city + " '" + address.zipcode + "'")
+    })
+}
+
+async function sortCards() {
+    let cards = document.querySelectorAll(".user");
+    console.log(cards.length)
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].remove();
+    }
+
+    let userObjs = await loadUsers();
+    console.log(userObjs.sort(function(a,b){
+        let textA = a.username;
+        let textB = b.username;
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    }))
+
+
+    let wrapper = document.querySelector(".row");
+    userObjs.forEach(userObj=>
+    {
+        let userCard= document.createElement("div");
+        userCard.className="col-lg-3 col-md-6 col-sm-12 mt-4";
+        userCard.innerHTML= `<div class="card" style="width: 16rem;">
+        <div class="card-body">
+        <a href="detail.html?id=${userObj.name}|${userObj.username}|${userObj.email}" style="color: black; text-decoration: none">
+        <h5 class="card-title">${userObj.name}</h5></a>
+          <h6 class="card-subtitle mb-2 text-muted">${userObj.username}</h6>
+          <p class="card-text">${userObj.email}</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>`
+      wrapper.appendChild(userCard);
+    })
+}
+ /* document.getElementById("buttonSearch").addEventListener("click", function(event){
+    event.preventDefault()
+  });*/
+  /*document.getElementById("select").addEventListener("change", function(event){
+    event.preventDefault()
+    console.log(event.target.value);
+  });
+
+
+
+  async function search(input){
+    let obj = await loadUsers();
+    let searchedobj =  obj.filter(function(input) {
+        return obj.name == input;
+    });
+    .map(foundKey => ({...obj[foundKey], key: foundKey }))
+  }
+
+
+  document.getElementById("search").addEventListener("change", function(event){
+    event.preventDefault()
+    let inputSearch= event.target.value;
+  });
+  
+ document.getElementById("buttonSearch").addEventListener("click", async function getFilteredUser(event){
+    event.preventDefault()
+      let input = document.getElementById("search").value;
+      const result = search(input);
+      console.log(result);
+}
+);
+*/
+
+async function sortCards() {
+    let cards = document.querySelectorAll(".user");
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].remove();
+    }
+
+    let userObjs = await loadUsers();
+    console.log(userObjs.sort(function(a,b){
+        let textA = a.username;
+        let textB = b.username;
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    }))
+    let wrapper = document.querySelector(".row");
+    userObjs.forEach(userObj=>
+    {
+        let userCard= document.createElement("div");
+        userCard.className="col-lg-3 col-md-6 col-sm-12 mt-4";
+        userCard.innerHTML=  `<div class="card" style="width: 16rem;">
+        <div class="card-body">
+        <h5 class="card-title">${userObj.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${userObj.username}</h6>
+          <p class="card-text">${userObj.email}</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div> `
+      wrapper.appendChild(userCard);
+    })
+}
+
